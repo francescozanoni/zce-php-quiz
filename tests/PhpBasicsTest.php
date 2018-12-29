d092284993dd4e09ccdb3a358d8918000c3e0410
Francesco Zanoni <francescozanoni81@gmail.com>
2018-12-29 11:21:42 +0100

    Tests: refactored common logic to base QuizTestCase

--- tests/PhpBasicsTest.php
+++ tests/PhpBasicsTest.php
@@ -1,19 +1,18 @@
 <?php
-use PHPUnit\Framework\TestCase;
+namespace Tests;
 
-class PhpBasicsTest extends TestCase
+class PhpBasicsTest extends QuizTestCase
 {
     public function testSyntax()
     {
         $xmlFilePath = __DIR__ . '/../src/php_basics/syntax.xml';
-        $xsdFilePath = __DIR__ . '/../schema.xsd';
-
+        
         // Enable user error handling
         libxml_use_internal_errors(true);
 
         $xml = new DOMDocument();
         $xml->load($xmlFilePath);
 
-        $this->assertTrue($xml->schemaValidate($xsdFilePath));
+        $this->assertTrue($xml->schemaValidate($this->xsdFilePath));
     }
 }
---
+++ tests/QuizTestCase.php
@@ -0,0 +1,9 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class QuizTestCase extends TestCase
+{
+  public $xsdFilePath = __DIR__ . '/../schema.xsd';
+}
